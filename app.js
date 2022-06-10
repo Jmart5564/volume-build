f298ef41f8cfa60887b2932bac9680bac724f096
Jmart5564 <Jmart5564@gmail.com>
2022-06-10 12:49:36 -0700

    volume goes up and down

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
 
 // state
+let volume = 10;
+
 
 
 // component
     // define and grab DOM elements
     // display functions
+function changeVolume(input) {
+
+    if (input === 'up') {
+        volume++;
+    }
+    if (input === 'down') {
+        volume--;
+    }
+    displayVolume();
+}
+
     // subscribe to events
+
+const downButton = document.getElementById('volume-down');
+downButton.addEventListener('click', () => {
+    changeVolume('down');
+});
+
+
+const upButton = document.getElementById('volume-up');
+upButton.addEventListener('click', () => {
+    changeVolume('up');
+});
+
         // event handlers - what needs to happen?
         // logic and calculations
         // state update
         // re-display components
+const volumeDisplay = document.getElementById('volume');
 
+function displayVolume() {
+    volumeDisplay.textContent = volume;
+}
 
 // page load actions
+displayVolume();
